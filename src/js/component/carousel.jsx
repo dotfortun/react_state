d841a249822b6d1c83e8719efc975fd4d1780296
dotfortun <shane.bell.dev@gmail.com>
2022-06-15 22:27:05 +0000

    sdafsadfa

---
+++ src/js/component/carousel.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+
+// https://getbootstrap.com/docs/5.2/components/carousel/
+
+const CarouselItem = ({ src, alt }) => {
+  return (
+    <div className="carousel-item active">
+      <img src={src} className="d-block w-100" alt={alt} />
+    </div>
+  );
+};
+
+// <Carousel src="" alt="" />
+
+const Carousel = ({ images }) => {
+  return (
+    <div
+      id="carouselExampleIndicators"
+      className="carousel slide"
+      data-bs-ride="true"
+    >
+      <div className="carousel-indicators">
+        <button
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide-to="0"
+          className="active"
+          aria-current="true"
+          aria-label="Slide 1"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide-to="1"
+          aria-label="Slide 2"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleIndicators"
+          data-bs-slide-to="2"
+          aria-label="Slide 3"
+        ></button>
+      </div>
+      <div className="carousel-inner">
+        {new Array(images).map((elem, idx) => {
+          return <Carousel key={idx} src={elem} />;
+        })}
+      </div>
+      <button
+        className="carousel-control-prev"
+        type="button"
+        data-bs-target="#carouselExampleIndicators"
+        data-bs-slide="prev"
+      >
+        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Previous</span>
+      </button>
+      <button
+        className="carousel-control-next"
+        type="button"
+        data-bs-target="#carouselExampleIndicators"
+        data-bs-slide="next"
+      >
+        <span className="carousel-control-next-icon" aria-hidden="true"></span>
+        <span className="visually-hidden">Next</span>
+      </button>
+    </div>
+  );
+};
+
+export { Carousel, CarouselItem };
--- src/js/index.js
+++ src/js/index.js
@@ -10,7 +10,19 @@ import Home from "./component/home.jsx";
 import Counter from "./component/counter.jsx";
 import Form from "./component/formSubmission.jsx";
 import TrafficLight from "./component/trafficlight.jsx";
+import { Carousel } from "./component/carousel.jsx";
 
+const img = [
+  "https://placekitten.com/200",
+  "https://placekitten.com/200",
+  "https://placekitten.com/200",
+  "https://placekitten.com/200",
+];
 
 //render your react application
-ReactDOM.render(<TrafficLight />, document.querySelector("#app"));
+ReactDOM.render(
+  <Carousel
+    images={img}
+  />,
+  document.querySelector("#app")
+);
